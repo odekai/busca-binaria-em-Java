d16580e0b396bda643a75e58f76c480da50a99d1
odekai <vilasjoaovitor71@gmail.com>
2024-10-31 15:33:48 -0300

    crianda logica de busca e testando

---
+++ buscaBinaria.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class buscaBinaria {
+   public  int inicio;
+   public  int start;
+    public int fim; 
+
+
+ 
+
+
+
+
+    // funcão pra achar o numero 7
+
+    public void buscar(int target){
+        target= 7;
+        inicio = 1;
+        start = 5;
+        fim =.length(-1);
+    
+        
+    }
+
+
+
+}
---
+++ testando.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class testando {
+    public static void main(String[] args) {
+         ArrayList<Integer> listaDenumeros = new ArrayList<>(Arrays.asList (0,1,2,3,4,5,6,7,8,9,10));
+     
+    }
+    
+}
